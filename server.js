beedccfbc4cafaab48471dab1f96d6d96f0d8c2f
kasundi2002 <kasundi2002@gmail.com>
2025-02-24 10:16:21 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+//Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log(err));
