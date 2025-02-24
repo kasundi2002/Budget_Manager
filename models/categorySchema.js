977cb8e4d6aa21d9191dc0a95ccc895297ea286d
kasundi2002 <kasundi2002@gmail.com>
2025-02-24 23:38:59 +0530

    frontend ad backend folder added

--- models/categorySchema.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const categorySchema = new mongoose.Schema({
-    name: { type: String, unique: true, required: true },
-    type: { type: String, enum: ["income", "expense"], required: true }  // Defines if it's for income or expenses
-});
-
-export default mongoose.model("Category", categorySchema);
