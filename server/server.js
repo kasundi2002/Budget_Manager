6c932a3d955844fab57896fdb822a3d228a4d831
it22221582 <it22221582@my.sliit.lk>
2025-03-10 22:51:38 +0530

    update 2

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const helmet = require("helmet");
+
 require("dotenv").config();
 
 const app = express();
@@ -8,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Middleware
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
